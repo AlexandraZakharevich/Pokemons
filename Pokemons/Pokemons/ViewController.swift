285d851183cc3e4e44842f8e439fd06fae39184f
Alexandra Zakharevich <alexa.zakharevich_iosdev@mail.ru>
2022-11-15 17:03:43 +0300

    create project

---
+++ Pokemons/Pokemons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemons
+//
+//  Created by Александра Захаревич on 15.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
