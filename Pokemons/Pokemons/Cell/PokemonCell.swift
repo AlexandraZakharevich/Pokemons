e9207d9f6d07b2c26af3e768d905ca8509fcb168
Alexandra Zakharevich <alexa.zakharevich_iosdev@mail.ru>
2022-11-15 17:45:12 +0300

    add pokemonTableView

---
+++ Pokemons/Pokemons/Cell/PokemonCell.swift
@@ -0,0 +1,54 @@
+//
+//  PokemonCell.swift
+//  Pokemons
+//
+//  Created by Александра Захаревич on 15.11.22.
+//
+
+import UIKit
+
+class PokemonCell: UITableViewCell {
+    
+    let pokemonNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .italicSystemFont(ofSize: 20)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Pokemon Name"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        setupConstraints()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+private extension PokemonCell {
+    func setup() {
+        pokemonNameLabel.backgroundColor = .white
+        addSubview(pokemonNameLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            pokemonNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            pokemonNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            pokemonNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 15),
+            pokemonNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 5)
+        ])
+    }
+}
--- Pokemons/Pokemons/ViewController.swift
+++ Pokemons/Pokemons/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let pokemonCell = "PokemonCell"
+    private let pokemonsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.bounces = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+        setDelegaties()
+        
+        pokemonsTableView.register(PokemonCell.self, forCellReuseIdentifier: pokemonCell)
+    }
+    
+}
+private extension ViewController {
+    func setupViews() {
+        pokemonsTableView.backgroundColor = .blue
+        view.addSubview(pokemonsTableView)
         
     }
-
-
+    
+    func setDelegaties() {
+        pokemonsTableView.delegate = self
+        pokemonsTableView.dataSource = self
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            pokemonsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            pokemonsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            pokemonsTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            pokemonsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: pokemonCell, for: indexPath) as! PokemonCell
+        return cell
+    }
+}
